8506fd70b3cdf346928172d485613fac49583e23
DhyeyaDesai <47080849+DhyeyaDesai@users.noreply.github.com>
2021-07-17 22:28:45 +0530

    testing

---
+++ testfile.py
@@ -0,0 +1,9 @@
+
+# using an access token
+# g = Github('rahuldesai1999@gmail.com', '538049682220Ba')
+
+g = Github('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')
+
+# hostname = 'github.com/DhyeyaDesai'
+# # Github Enterprise with custom hostname
+# g = Github(base_url=f"https://{hostname}/api/v3", login_or_token="access_token")
